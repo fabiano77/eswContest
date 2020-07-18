506eecbad2ff8ac24f27fb7b19f4526acc112ef2
Kim Daehee <fabiano7@naver.com>
2020-07-19 04:25:55 +0900

    실제 알고리즘 테스트할 코드 작성

---
+++ CarSDK/Application/7_all_test/imgProcess.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+#include <stdio.h>
+#include <string.h>
+//#include <sys/time.h>
+
+#include <opencv2/opencv.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+//#include <opencv2/gpu/device/utility.hpp>
+#include <opencv2/core/core.hpp>
+#include <opencv2/objdetect/objdetect.hpp>
+
+#define PI 3.1415926
+
+using namespace std;
+using namespace cv;
+
+extern "C" {
+
+
+	/**********************
+	* 영상처리 알고리즘 작성
+	***********************/
+
+
+
+
+
+
+
+
+
+
+}
---
+++ CarSDK/Application/7_all_test/imgProcess.h
@@ -0,0 +1,17 @@
+#ifndef IMGPROCESS_H_
+#define IMGPROCESS_H_ 
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/****************************
+* 영상처리 함수 작성
+*****************************/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif //IMGPROCESS_H_
+

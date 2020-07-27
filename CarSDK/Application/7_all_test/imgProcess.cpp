2f45c2d0dbd3bd96074a3575dc45ac053782004c
Kim Daehee <fabiano7@naver.com>
2020-07-27 23:41:53 +0900

    깨진 글자 복원

--- CarSDK/Application/7_all_test/imgProcess.cpp
+++ CarSDK/Application/7_all_test/imgProcess.cpp
@@ -19,34 +19,34 @@ using namespace std;
 using namespace cv;
 
 int calculSteer(Mat& src);
-//PreCondition	: frame�� �ڵ����� topview2������ �Է����� �޴´�.
-//PostCondition	: ������ �ν��Ͽ� �νĵ� ����� src�� ǥ���Ѵ�..
-//Return : �����ؾߵ� ������� ��ȯ�Ѵ�(-500 ~ 500)
+//PreCondition	: frame은 자동차의 topview2영상을 입력으로 받는다.
+//PostCondition	: 차선을 인식하여 인식된 결과를 src에 표시한다..
+//Return : 조향해야될 결과값을 반환한다(-500 ~ 500)
 
 void lineFiltering(Mat& src, Mat& dst, int mode);
-//PreCondition	: ����
-//PostCondition	: �����(or ���)������ �����ϴ� src���󿡼� ���������� ���� ���͸��Ͽ� dst�� ��ȯ�Ѵ�.
-//Return : ����
+//PreCondition	: 없음
+//PostCondition	: 노란색(or 흰색)차선이 존재하는 src영상에서 차선영역만 남게 필터링하여 dst로 반환한다.
+//Return : 없음
 
 void houghLineDetection(Mat& src, Mat& dst, bool printMode);//, Vec4i& firstLine, Vec4i& secondLine);
-//PreCondition	: ����.
-//PostCondition	: houghLineP �Լ� ����� dst�� ��Ÿ���ش�.
-//Return : ����
+//PreCondition	: 없음.
+//PostCondition	: houghLineP 함수 결과를 dst에 나타내준다.
+//Return : 없음
 
 void cannyEdge(Mat& src, Mat& dst);
-//PreCondition	: ����
-//PostCondition	: canny�Լ� ����� dst�� ����Ѵ�.
-//Return : ����
+//PreCondition	: 없음
+//PostCondition	: canny함수 결과를 dst로 출력한다.
+//Return : 없음
 
 Vec4i ransacLine(Mat& src, Mat& dst, int w, int h, double T, int n, bool printMode);
-//PreCondition	: src������ cannyó���� ��ģ ��������Ѵ�.
-//PostCondition	: dst�� ����� ������ ��ũ������ ǥ���Ѵ�.
-//Return : ����� ������ ��ȯ(Vec4i)
+//PreCondition	: src영상은 canny처리를 거친 결과여야한다.
+//PostCondition	: dst에 검출된 직선을 핑크색으로 표시한다.
+//Return : 검출된 라인이 반환(Vec4i)
 
 Vec8i split_ransacLine(Mat& src, Mat& dst, int w, int h, double T, int n, bool printMode);
-//PreCondition	: src������ cannyó���� ��ģ ��������Ѵ�.
-//PostCondition	: src������ �¿�� �����Ͽ� �����Ѵ�. �׿� ����.
-//Return : ����� ���� �ΰ��� ��ȯ(Vec8i)
+//PreCondition	: src영상은 canny처리를 거친 결과여야한다.
+//PostCondition	: src영상을 좌우로 분할하여 검출한다. 그외 동일.
+//Return : 검출된 라인 두개를 반환(Vec8i)
 //			return Vec8i(leftLine[0], leftLine[1], leftLine[2], leftLine[3],
 //						rightLine[0], rightLine[1], rightLine[2], rightLine[3]);
 

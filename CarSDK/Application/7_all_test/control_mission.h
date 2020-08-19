0cf5dc75e7b0effb1a36c7ff1c707ddd1c508476
smlee212 <63399455+smlee212@users.noreply.github.com>
2020-08-19 23:13:29 +0900

    add Tunnel, RoundAbout

--- CarSDK/Application/7_all_test/control_mission.h
+++ CarSDK/Application/7_all_test/control_mission.h
@@ -5,10 +5,6 @@
 extern "C" {
 #endif
 
-
-
-
-
 int DistanceSensor_cm(int channel);
 //Return : 적외선 센서 값(cm)을 반환한다. (거리가 가까울수록 숫자높음)
 
@@ -22,11 +18,18 @@ void DesiredDistance(int SettingSpeed, int SettingDistance);
 // Postcondition : SettingSpeed의 속도(양수는 전진, 음수는 후진)로 SettingDistance의 step 만큼 움직인다.
 
 
-
-
-
 #ifdef __cplusplus
 }
+
+int RoundAbout_isStart(const int Distance1);
+// Return : 1 이면 다음 분기로 진행
+
+int RoundAbout_isDelay(const int Distance1, int& speed);
+// return 1 이면 정지
+
+int RoundAbout_End(const int Distance1, const int Distance2);
+// return 1 이면 분기 빠져나감
+
 #endif /* __cplusplus */
 
 #endif //CONTROL_MISSION_H

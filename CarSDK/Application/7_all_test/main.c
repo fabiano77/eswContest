0ee5e561ff754d0075ebffdbfe98f548421a24e7
MinsooKang <3neutronstar@gmail.com>
2020-08-22 12:21:38 +0900

    add adt

--- CarSDK/Application/7_all_test/main.c
+++ CarSDK/Application/7_all_test/main.c
@@ -1147,10 +1147,11 @@ void* mission_thread(void* arg)
 			}
 		}
 
-		if (finish)
+		if (finish)/*MS*/
 		{
-			if (0)
-			{
+			if (0)/*노란색 가로 직선이 일정이하로 떨어지면 입력*/
+			{//Encoder 사용해서 일정 직진하면 종료하게 설정
+				//끝나고 삐소리 
 				data->imgData.bmission = true;
 				data->imgData.bprintString = true;
 				sprintf(data->imgData.missionString, "finish");

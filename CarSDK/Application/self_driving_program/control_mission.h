d2636958c94f250a4b2a768d4603f403cf41452c
3neutronstar <minsoo.kang0918@gmail.com>
2020-11-19 20:29:49 +0900

    change the remark in UTF-8

--- CarSDK/Application/self_driving_program/control_mission.h
+++ CarSDK/Application/self_driving_program/control_mission.h
@@ -79,15 +79,15 @@ extern "C"
 
 	enum CameraVerticalState
 	{
-		CAMERA_UP,	//?¥?• ë¬? ?¸?‹?„ ?œ„?•´ ?˜¬ë¦°ìƒ?ƒœ
-		CAMERA_DOWN //?›?˜ ?ƒ?ƒœ -->?´ ë¶?ë¶? ì¡°ì • ?•„?š” MS
+		CAMERA_UP,	//ì¥ì• ë¬¼ ì¸ì‹ì„ ìœ„í•´ ì˜¬ë¦°ìƒíƒœ
+		CAMERA_DOWN //ì›ë˜ ìƒíƒœ -->ì´ ë¶€ë¶„ ì¡°ì • í•„ìš” MS
 	};
 
 	enum DirectionState
 	{
 		LEFT,
 		RIGHT,
-		STOP //?•?— ?¥?• ë¬¼ì´ ?ˆ?‹¤ë©? ?Š¤?ƒ‘(overtaking?´ on?¼ ?•Œë§?)
+		STOP //ì•ì— ì¥ì• ë¬¼ì´ ìˆë‹¤ë©´ ìŠ¤íƒ‘(overtakingì´ onì¼ ë•Œë§Œ)
 	};
 
 	enum SignalLightState
@@ -103,24 +103,24 @@ extern "C"
 	{
 		bool frontRight;
 		bool rearRight;
-		bool bparking;		 // ì£¼ì°¨ ì¤? ê±°ë¦¬ ? •ë³? ì¶œë ¥?„ ?œ„?•œ ë³??ˆ˜
-		bool verticalFlag;	 // ?ˆ˜ì§? ì£¼ì°¨ ?™œ?„±?™”ë¥? ?‚˜????‚´?Š” ?”Œ?˜ê·?
-		bool horizontalFlag; // ?ˆ˜?‰ ì£¼ì°¨ ?™œ?„±?™”ë¥? ?‚˜????‚´?Š” ?”Œ?˜ê·?
+		bool bparking;		 // ì£¼ì°¨ ì¤‘ ê±°ë¦¬ ì •ë³´ ì¶œë ¥ì„ ìœ„í•œ ë³€ìˆ˜
+		bool verticalFlag;	 // ìˆ˜ì§ ì£¼ì°¨ í™œì„±í™”ë¥¼ ë‚˜íƒ€ë‚´ëŠ” í”Œë˜ê·¸
+		bool horizontalFlag; // ìˆ˜í‰ ì£¼ì°¨ í™œì„±í™”ë¥¼ ë‚˜íƒ€ë‚´ëŠ” í”Œë˜ê·¸
 	};
 
 	struct Overtaking
 	{
-		bool sideSensorFlag;				   // ì°¨ëŸ‰?˜ ?‚¬?´?“œ ?ƒì§? ?™œ?„±?™” ?”Œ?˜ê·?
-		enum DirectionState headingDirection;  //ì°¨ëŸ‰?˜ ?´?™ë°©í–¥ ê²°ì •
-		enum CameraVerticalState updownCamera; //ì¹´ë©”?¼ë¥? ?œ„ë¡? ?˜¬ë¦´ì?? ë§ì?? ê²°ì •?•˜?Š” ë¶?ë¶?
+		bool sideSensorFlag;				   // ì°¨ëŸ‰ì˜ ì‚¬ì´ë“œ íƒì§€ í™œì„±í™” í”Œë˜ê·¸
+		enum DirectionState headingDirection;  //ì°¨ëŸ‰ì˜ ì´ë™ë°©í–¥ ê²°ì •
+		enum CameraVerticalState updownCamera; //ì¹´ë©”ë¼ë¥¼ ìœ„ë¡œ ì˜¬ë¦´ì§€ ë§ì§€ ê²°ì •í•˜ëŠ” ë¶€ë¶„
 		char leftFlag;
 		char rightFlag;
 	};
 
 	struct Finish
 	{
-		int distEndLine; //ê²°ìŠ¹?„ ê¹Œì???˜ ê±°ë¦¬
-		bool checkFront; //?•?˜ ?ˆ˜ì§? ?…¸????„  ?ŒŒ?•…
+		bool checkFront; //ì•ì˜ ìˆ˜ì§ ë…¸ë€ì„  íŒŒì•…
+		int distEndLine; //ê²°ìŠ¹ì„ ê¹Œì§€ì˜ ê±°ë¦¬
 	};
 
 	struct SignalLight
@@ -134,193 +134,194 @@ extern "C"
 	/******************** thread struct ********************/
 	struct MissionData
 	{
-		struct Parking parkingData;			// ì£¼ì°¨?— ?•„?š”?•œ ?”Œ?˜ê·¸ë?? ?‹´?Š” êµ¬ì¡°ì²?
-		struct Overtaking overtakingData;	// ì¶”ì›”?— ?•„?š”?•œ ?”Œ?˜ê·? ?‹´?Š” êµ¬ì¡°ì²?
-		struct SignalLight signalLightData; // ?‹ ?˜¸?“±?— ?•„?š”?•œ ë³??ˆ˜ë¥? ?‹´?Š” êµ¬ì¡°ì²?
-		struct Finish finishData;
-		enum MissionState ms[9];
-		uint32_t loopTime; // mission ?Š¤? ˆ?“œ ë£¨í”„ ?‹œê°?
-
-		int frame_priority;
-		int finish_distance;
+		uint32_t loopTime; // mission ìŠ¤ë ˆë“œ ë£¨í”„ ì‹œê°„
 		bool broundabout;
 		bool btunnel;
-		bool overtakingFlag; // ì¶”ì›”ì°¨ë¡œ ?”Œ?˜ê·? ->MS ?´?›„ overtaking struct ì¶”ê???•  ê²?
+		bool overtakingFlag; // ì¶”ì›”ì°¨ë¡œ í”Œë˜ê·¸ ->MS ì´í›„ overtaking struct ì¶”ê°€í•  ê²ƒ
 		bool changeMissionState;
 		bool checkWhiteLineFlag;
+		int frame_priority;
+		int finish_distance;
+
+		struct Parking parkingData;			// ì£¼ì°¨ì— í•„ìš”í•œ í”Œë˜ê·¸ë¥¼ ë‹´ëŠ” êµ¬ì¡°ì²´
+		struct Overtaking overtakingData;	// ì¶”ì›”ì— í•„ìš”í•œ í”Œë˜ê·¸ ë‹´ëŠ” êµ¬ì¡°ì²´
+		struct SignalLight signalLightData; // ì‹ í˜¸ë“±ì— í•„ìš”í•œ ë³€ìˆ˜ë¥¼ ë‹´ëŠ” êµ¬ì¡°ì²´
+		struct Finish finishData;
+		enum MissionState ms[9];
 	};
 
 	struct ControlData
 	{
-		int steerVal;		// ì°¨ëŸ‰?˜ ?˜„?¬ ?•ë°”í?? ì¡°í–¥ê°?
-		int cameraY;		// ì°¨ëŸ‰?˜ ?˜„?¬ ì¹´ë©”?¼ê°ë„
-		int desireSpeedVal; // ì°¨ëŸ‰?˜ ?˜„?¬ ?†?„
-		int beforeSpeedVal; // ì°¨ëŸ‰?˜ ?´? „ ?†?„
+		int steerVal;		// ì°¨ëŸ‰ì˜ í˜„ì¬ ì•ë°”í€´ ì¡°í–¥ê°
+		int cameraY;		// ì°¨ëŸ‰ì˜ í˜„ì¬ ì¹´ë©”ë¼ê°ë„
+		int desireSpeedVal; // ì°¨ëŸ‰ì˜ í˜„ì¬ ì†ë„
+		int beforeSpeedVal; // ì°¨ëŸ‰ì˜ ì´ì „ ì†ë„
 		int settingSpeedVal;
-		unsigned short lightFlag; // ì°¨ëŸ‰?˜ ?˜„?¬ ? „ì¡°ë“±?ƒ?ƒœ
+		unsigned short lightFlag; // ì°¨ëŸ‰ì˜ í˜„ì¬ ì „ì¡°ë“±ìƒíƒœ
 	};
 
 	struct ImgProcessData
 	{
-		char missionString[50]; // ?˜¤ë²„ë ˆ?´?— ?‘œ?‹œ?•  ë¬¸ì?—´
-		int debugMode;			// ?””ë²„ê·¸ ëª¨ë“œ(0~ 9)
-		int topMode;			// ?ƒ‘ë·? ëª¨ë“œ (0, 1, 2)
-		uint32_t loopTime;		// img ?Š¤? ˆ?“œ ë£¨í”„ ?‹œê°?
-		bool dump_request;		// ?¤?”„?š”ì²?
+		uint32_t loopTime; // img ìŠ¤ë ˆë“œ ë£¨í”„ ì‹œê°„
+		bool dump_request; // ë¤í”„ìš”ì²­
 		bool bskip;
-		bool bvideoRecord;	// ?™?˜?ƒ ?…¹?™” ?‹œ?‘
-		bool bvideoSave;	// ?™?˜?ƒ ?ŒŒ?¼ ????¥
-		bool bcalibration;	// ìº˜ë¦¬ë¸Œë ˆ?´?…˜ ON/OFF
-		bool bdebug;		// ?””ë²„ê·¸ëª¨ë“œ ON/OFF
-		bool btopview;		// ?ƒ‘ë·? ON/OFF
-		bool bmission;		// ë¯¸ì…˜ì§„ì… ON/OFF (ì°¨ì„ ?¸?‹ ?‚¬?š©?•˜ì§? ?•Šê²Œë¨)
-		bool bauto;			// ??™ ì¡°í–¥ ON/OFF
-		bool bspeedControl; // ??™ ì¡°í–¥?˜ ?†?„ê°œì… ON/OFF
-		bool bwhiteLine;	// ??™ ì¡°í–¥?˜ ?°?ƒ‰ ?„  ?ƒì§? ON/OFF
-		bool bprintString;	// ?˜¤ë²„ë ˆ?´?— ë¬¸ì?—´ ?‘œ?‹œ ON/OFF
-		bool bprintMission; // ?˜¤ë²„ë ˆ?´?— ë¯¸ì…˜? •ë³? ?‘œ?‹œ
-		bool bprintSensor;	// ?˜¤ë²„ë ˆ?´?— ?„¼?„œê°? ?‘œ?‹œ ON/OFF
-		bool bprintTire;	// ?˜¤ë²„ë ˆ?´?— ë°”í?´ê°?„ ?‘œ?‹œ
-		bool bdark;			// ?„°?„ ?ƒì§? ON/OFF
+		bool bvideoRecord;	// ë™ì˜ìƒ ë…¹í™” ì‹œì‘
+		bool bvideoSave;	// ë™ì˜ìƒ íŒŒì¼ ì €ì¥
+		bool bcalibration;	// ìº˜ë¦¬ë¸Œë ˆì´ì…˜ ON/OFF
+		bool bdebug;		// ë””ë²„ê·¸ëª¨ë“œ ON/OFF
+		bool btopview;		// íƒ‘ë·° ON/OFF
+		bool bmission;		// ë¯¸ì…˜ì§„ì… ON/OFF (ì°¨ì„ ì¸ì‹ ì‚¬ìš©í•˜ì§€ ì•Šê²Œë¨)
+		bool bauto;			// ìë™ ì¡°í–¥ ON/OFF
+		bool bspeedControl; // ìë™ ì¡°í–¥ì˜ ì†ë„ê°œì… ON/OFF
+		bool bwhiteLine;	// ìë™ ì¡°í–¥ì˜ í°ìƒ‰ ì„  íƒì§€ ON/OFF
+		bool bprintString;	// ì˜¤ë²„ë ˆì´ì— ë¬¸ìì—´ í‘œì‹œ ON/OFF
+		bool bprintMission; // ì˜¤ë²„ë ˆì´ì— ë¯¸ì…˜ì •ë³´ í‘œì‹œ
+		bool bprintSensor;	// ì˜¤ë²„ë ˆì´ì— ì„¼ì„œê°’ í‘œì‹œ ON/OFF
+		bool bprintTire;	// ì˜¤ë²„ë ˆì´ì— ë°”í€´ê°ë„ í‘œì‹œ
+		bool bdark;			// í„°ë„ íƒì§€ ON/OFF
 		bool bcheckFrontWhite;
-		bool bcheckPriority;	// ?š°?„ ? •ì§? ?‘œì§??Œ ?ƒì§? ON/OFF
-		bool bcheckSignalLight; // ?‹ ?˜¸?“± ?ƒì§? ON/OFF
-		bool bcheckFinishLine;	// ?”¼?‹ˆ?‹œ?¼?¸ ?ƒì§? ON/OFF
+		bool bcheckPriority;	// ìš°ì„ ì •ì§€ í‘œì§€íŒ íƒì§€ ON/OFF
+		bool bcheckSignalLight; // ì‹ í˜¸ë“± íƒì§€ ON/OFF
+		bool bcheckFinishLine;	// í”¼ë‹ˆì‹œë¼ì¸ íƒì§€ ON/OFF
+		char missionString[30]; // ì˜¤ë²„ë ˆì´ì— í‘œì‹œí•  ë¬¸ìì—´
+		int topMode;			// íƒ‘ë·° ëª¨ë“œ (0, 1, 2)
+		int debugMode;			// ë””ë²„ê·¸ ëª¨ë“œ(0~ 9)
 	};
 
 	// DistanceSensor_cm
-	// ? ?™¸?„  ?„¼?„œ?—?„œ ë¬¼ì²´????˜ ê±°ë¦¬ë¥? ì¸¡ì •?•´ì£¼ëŠ” ?•¨?ˆ˜
+	// ì ì™¸ì„  ì„¼ì„œì—ì„œ ë¬¼ì²´ì™€ì˜ ê±°ë¦¬ë¥¼ ì¸¡ì •í•´ì£¼ëŠ” í•¨ìˆ˜
 	// PreCondition 	: none
 	// PostCondition	: none
-	// Return 		: ? ?™¸?„  ?„¼?„œ?—?„œ ì¸¡ì •?•œ ê±°ë¦¬ ê°’ì„ ë°˜í™˜?•œ?‹¤.
+	// Return 		: ì ì™¸ì„  ì„¼ì„œì—ì„œ ì¸¡ì •í•œ ê±°ë¦¬ ê°’ì„ ë°˜í™˜í•œë‹¤.
 	int DistanceSensor_cm(int channel);
 
 	// sensor_dist
-	// ? ?™¸?„  ?„¼?„œ?—?„œ ì¸¡ì •?•œ ê±°ë¦¬ ê°’ì„ ? •ê·œí™”?‹œ?‚¤?Š” ?•¨?ˆ˜?´?‹¤.
+	// ì ì™¸ì„  ì„¼ì„œì—ì„œ ì¸¡ì •í•œ ê±°ë¦¬ ê°’ì„ ì •ê·œí™”ì‹œí‚¤ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
 	// PostCondition	: none
-	// Return 		: ? ?™¸?„  ?„¼?„œë¡? ì¸¡ì •?•œ ê±°ë¦¬ ê°’ì„ ? •ê·œí™”?•˜?—¬ ë°˜í™˜?•œ?‹¤.
+	// Return 		: ì ì™¸ì„  ì„¼ì„œë¡œ ì¸¡ì •í•œ ê±°ë¦¬ ê°’ì„ ì •ê·œí™”í•˜ì—¬ ë°˜í™˜í•œë‹¤.
 	int sensor_dist(int channel, int input);
 
 	// Encoder_Read
-	// ì°¨ëŸ‰?´ ?´?™?•œ ê±°ë¦¬ë¥? ì¸¡ì •?•´ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
+	// ì°¨ëŸ‰ì´ ì´ë™í•œ ê±°ë¦¬ë¥¼ ì¸¡ì •í•´ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
 	// PostCondition	: none
-	// Return 		: ?˜„?¬ê¹Œì?? ì°¨ëŸ‰?´ ?´?™?•œ ê±°ë¦¬ë¥? ë°˜í™˜?•œ?‹¤.
+	// Return 		: í˜„ì¬ê¹Œì§€ ì°¨ëŸ‰ì´ ì´ë™í•œ ê±°ë¦¬ë¥¼ ë°˜í™˜í•œë‹¤.
 	signed int Encoder_Read(void);
 
 	// StopLine
-	// ?•˜?‹¨ ? ?™¸?„  ?„¼?„œë¡? ?°?ƒ‰ ?¼?¸?¸ì§? ?Œë³„í•´ì£¼ëŠ” ?•¨?ˆ˜
+	// í•˜ë‹¨ ì ì™¸ì„  ì„¼ì„œë¡œ í°ìƒ‰ ë¼ì¸ì¸ì§€ íŒë³„í•´ì£¼ëŠ” í•¨ìˆ˜
 	// PreCondition 	: none
 	// PostCondition	: none
-	// Return 		: 7ê°œì˜ ? ?™¸?„  ?„¼?„œ ì¤? ?°?ƒ‰?„ ê°ì???•œ ?„¼?„œ?˜ ê°œìˆ˜ê°? Lineflagë³´ë‹¤ ë§ìœ¼ë©? 1, ?•„?‹?‹œ 0
+	// Return 		: 7ê°œì˜ ì ì™¸ì„  ì„¼ì„œ ì¤‘ í°ìƒ‰ì„ ê°ì§€í•œ ì„¼ì„œì˜ ê°œìˆ˜ê°€ Lineflagë³´ë‹¤ ë§ìœ¼ë©´ 1, ì•„ë‹ì‹œ 0
 	int StopLine(int Lineflag);
 
 	// SteeringServo_Write_uart
-	// ì°¨ëŸ‰?˜ ?• ë°”í?´ë?? ?›?•˜?Š” ê°ë„(steerVal)ë¡? ì¡°í–¥?•´ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
+	// ì°¨ëŸ‰ì˜ ì• ë°”í€´ë¥¼ ì›í•˜ëŠ” ê°ë„(steerVal)ë¡œ ì¡°í–¥í•´ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: ì°¨ëŸ‰?˜ ?• ë°”í?´ëŠ” angleë¡? ì¡°í–¥?˜?–´ ?œ ì§??œ?‹¤.
-	//			uart ?†µ?‹  ?œ ë¬´ë?? ê°ì???•˜?—¬ ?†µ?‹ ?— ê°„ì„­?˜ì§? ?•Š?„ë¡? ???ê¸°í•œ?‹¤.
+	// PostCondition	: ì°¨ëŸ‰ì˜ ì• ë°”í€´ëŠ” angleë¡œ ì¡°í–¥ë˜ì–´ ìœ ì§€ëœë‹¤.
+	//			uart í†µì‹  ìœ ë¬´ë¥¼ ê°ì§€í•˜ì—¬ í†µì‹ ì— ê°„ì„­ë˜ì§€ ì•Šë„ë¡ ëŒ€ê¸°í•œë‹¤.
 	// Return 		: none
 	void SteeringServo_Write_uart(signed short angle);
 
 	// DesireSpeed_Write_uart
-	// ì°¨ëŸ‰?˜ ?†?„ë¥? ?›?•˜?Š” ?†?„(speed)ë¡? ë§ì¶°ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
+	// ì°¨ëŸ‰ì˜ ì†ë„ë¥¼ ì›í•˜ëŠ” ì†ë„(speed)ë¡œ ë§ì¶°ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: ì°¨ëŸ‰?˜ ?†?„?Š” speedê°? ?˜?–´ ?œ ì§??œ?‹¤.
-	//			uart ?†µ?‹  ?œ ë¬´ë?? ê°ì???•˜?—¬ ?†µ?‹ ?— ê°„ì„­?˜ì§? ?•Š?„ë¡? ???ê¸°í•œ?‹¤.
+	// PostCondition	: ì°¨ëŸ‰ì˜ ì†ë„ëŠ” speedê°€ ë˜ì–´ ìœ ì§€ëœë‹¤.
+	//			uart í†µì‹  ìœ ë¬´ë¥¼ ê°ì§€í•˜ì—¬ í†µì‹ ì— ê°„ì„­ë˜ì§€ ì•Šë„ë¡ ëŒ€ê¸°í•œë‹¤.
 	// Return 		: none
 	void DesireSpeed_Write_uart(signed short speed);
 
 	// DesiredDistance
-	// ?›?•˜?Š” ?†?„(speed)??? ê°ë„(steerVal)ë¥? ê°?ì§?ê³? ?¼? • ê±°ë¦¬(encoderVal)ë¥? ì£¼í–‰?•˜?Š” ?•¨?ˆ˜?´?‹¤.
+	// ì›í•˜ëŠ” ì†ë„(speed)ì™€ ê°ë„(steerVal)ë¥¼ ê°€ì§€ê³  ì¼ì • ê±°ë¦¬(encoderVal)ë¥¼ ì£¼í–‰í•˜ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: SettingSpeed?˜ ?†?„??? SettingSteering?˜ ê°ë„ë¥? ?œ ì§??•œì±„ë¡œ
-	//                    SettingDistance ë§Œí¼?˜ ê±°ë¦¬ë¥? ì£¼í–‰?•œ?‹¤.
+	// PostCondition	: SettingSpeedì˜ ì†ë„ì™€ SettingSteeringì˜ ê°ë„ë¥¼ ìœ ì§€í•œì±„ë¡œ
+	//                    SettingDistance ë§Œí¼ì˜ ê±°ë¦¬ë¥¼ ì£¼í–‰í•œë‹¤.
 	// Return 		: none
 	void DesiredDistance(int SettingSpeed, int SettingDistance, int SettingSteering);
 
 	// sleepDistance
-	// ?¼? • ê±°ë¦¬(encoderVal)ë¥? ì£¼í–‰?•˜?Š”?™?•ˆ ?œ ì§??•˜ê³ ìˆ?Š” ?•¨?ˆ˜?´?‹¤.
+	// ì¼ì • ê±°ë¦¬(encoderVal)ë¥¼ ì£¼í–‰í•˜ëŠ”ë™ì•ˆ ìœ ì§€í•˜ê³ ìˆëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: SettingDistance ë§Œí¼?˜ ê±°ë¦¬ë¥? ì£¼í–‰?›„ ?•¨?ˆ˜ë¥? ì¢…ë£Œ?•œ?‹¤(ì£¼í–‰??? ?œ ì§?).
+	// PostCondition	: SettingDistance ë§Œí¼ì˜ ê±°ë¦¬ë¥¼ ì£¼í–‰í›„ í•¨ìˆ˜ë¥¼ ì¢…ë£Œí•œë‹¤(ì£¼í–‰ì€ ìœ ì§€).
 	// Return 		: none
 	void sleepDistance(int SettingDistance);
 
 	// onlyDistance
-	// ?›?•˜?Š” ?†?„(speed)ë¥? ê°?ì§?ê³? ?¼? • ê±°ë¦¬(encoderVal)ë¥? ì£¼í–‰?•˜?Š” ?•¨?ˆ˜?´?‹¤.
+	// ì›í•˜ëŠ” ì†ë„(speed)ë¥¼ ê°€ì§€ê³  ì¼ì • ê±°ë¦¬(encoderVal)ë¥¼ ì£¼í–‰í•˜ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: SettingSpeed?˜ ?†?„ë¥? ?œ ì§??•œì±„ë¡œ SettingDistance ë§Œí¼?˜ ê±°ë¦¬ë¥? ì£¼í–‰?•œ?‹¤.
+	// PostCondition	: SettingSpeedì˜ ì†ë„ë¥¼ ìœ ì§€í•œì±„ë¡œ SettingDistance ë§Œí¼ì˜ ê±°ë¦¬ë¥¼ ì£¼í–‰í•œë‹¤.
 	// Return 		: none
 	void onlyDistance(int SettingSpeed, int SettingDistance);
 
 	// RoundAbout_isStart
-	// ?šŒ? „ êµì°¨ë¡œì—?„œ ì°¨ëŸ‰?´ ì§??‚˜ê°”ëŠ”ì§? ?™•?¸?•´ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
-	// PreCondition 	: ?šŒ? „ êµì°¨ë¡œì˜ ? •ì§??„ ?—?„œ ???ê¸°ì¤‘?´?—¬?•¼ ?•œ?‹¤.
-	// PostCondition	: 2.5ì´ˆë?? ???ê¸°í•œ ?›„?— ì£¼í–‰?„ ?‹œ?‘?•œ?‹¤.
-	// Return 		: ?•?— ì°¨ëŸ‰?´ ?‚˜????‚¬?‹¤ê°? ?—†?–´ì¡Œìœ¼ë©? 1, ?•„?‹?‹œ 0
+	// íšŒì „ êµì°¨ë¡œì—ì„œ ì°¨ëŸ‰ì´ ì§€ë‚˜ê°”ëŠ”ì§€ í™•ì¸í•´ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
+	// PreCondition 	: íšŒì „ êµì°¨ë¡œì˜ ì •ì§€ì„ ì—ì„œ ëŒ€ê¸°ì¤‘ì´ì—¬ì•¼ í•œë‹¤.
+	// PostCondition	: 2.5ì´ˆë¥¼ ëŒ€ê¸°í•œ í›„ì— ì£¼í–‰ì„ ì‹œì‘í•œë‹¤.
+	// Return 		: ì•ì— ì°¨ëŸ‰ì´ ë‚˜íƒ€ë‚¬ë‹¤ê°€ ì—†ì–´ì¡Œìœ¼ë©´ 1, ì•„ë‹ì‹œ 0
 	int RoundAbout_isStart(const int Distance1);
 
 	// Tunnel_isEnd
-	// ?„°?„?—?„œ ?‚˜?™”?Š”ì§? ?™•?¸?•´ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
-	// PreCondition 	: ?„°?„?— ì§„ì…?„ ?•œ ? ?´ ?ˆ?–´?•¼ ?•œ?‹¤.
-	// PostCondition	: ?„°?„?—?„œ ?‚˜?™”?œ¼ë¯?ë¡? ê¸°ë³¸ ì£¼í–‰?„ ?•´?•¼?•œ?‹¤.
-	// Return 		: ì¢?,?š° ?„¼?„œê°? ?¼? • ê±°ë¦¬ ?´?ƒ?˜ ê°’ì„ ê°?ì§?ë©? 1, ?•„?‹?‹œ 0
+	// í„°ë„ì—ì„œ ë‚˜ì™”ëŠ”ì§€ í™•ì¸í•´ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
+	// PreCondition 	: í„°ë„ì— ì§„ì…ì„ í•œ ì ì´ ìˆì–´ì•¼ í•œë‹¤.
+	// PostCondition	: í„°ë„ì—ì„œ ë‚˜ì™”ìœ¼ë¯€ë¡œ ê¸°ë³¸ ì£¼í–‰ì„ í•´ì•¼í•œë‹¤.
+	// Return 		: ì¢Œ,ìš° ì„¼ì„œê°€ ì¼ì • ê±°ë¦¬ ì´ìƒì˜ ê°’ì„ ê°€ì§€ë©´ 1, ì•„ë‹ì‹œ 0
 	int Tunnel_isEnd(const int Distance1, const int Distance2, const int Distance3, const int Distance4);
 
 	// Tunnel_SteerVal
-	// ?„°?„?—?„œ ì°¨ëŸ‰?˜ ?• ë°”í?´ë?? ì¡°í–¥?•´ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
-	// PreCondition 	: ?„°?„?— ì§„ì…?„ ?•œ ? ?´ ?ˆ?–´?•¼ ?•œ?‹¤.
-	// PostCondition	: ì°¨ëŸ‰?˜ ?• ë°”í?´ëŠ” angleë¡? ì¡°í–¥?˜?–´ ?œ ì§??œ?‹¤.
-	// Return 		: ì¢?,?š° ?„¼?„œ?—?„œ ì¸¡ì •?œ ê±°ë¦¬?˜ ì°¨ì´ë¥? ?†µ?•´ ? ? ˆ?•œ angle?„ ë°˜í™˜?•œ?‹¤.
+	// í„°ë„ì—ì„œ ì°¨ëŸ‰ì˜ ì• ë°”í€´ë¥¼ ì¡°í–¥í•´ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
+	// PreCondition 	: í„°ë„ì— ì§„ì…ì„ í•œ ì ì´ ìˆì–´ì•¼ í•œë‹¤.
+	// PostCondition	: ì°¨ëŸ‰ì˜ ì• ë°”í€´ëŠ” angleë¡œ ì¡°í–¥ë˜ì–´ ìœ ì§€ëœë‹¤.
+	// Return 		: ì¢Œ,ìš° ì„¼ì„œì—ì„œ ì¸¡ì •ëœ ê±°ë¦¬ì˜ ì°¨ì´ë¥¼ í†µí•´ ì ì ˆí•œ angleì„ ë°˜í™˜í•œë‹¤.
 	int Tunnel_SteerVal(const int Distance1, const int Distance2);
 
 	// frontLightOnOff
-	// ì°¨ëŸ‰?˜ ? „ë°? ?¼?´?Š¸ë¥? On, Off ?•´ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
+	// ì°¨ëŸ‰ì˜ ì „ë°© ë¼ì´íŠ¸ë¥¼ On, Off í•´ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: on?˜ ê°’ì— ?”°?¼ ì°¨ëŸ‰?˜ ? „ë°? ?¼?´?Š¸ê°? On ?˜?Š” Offê°? ?œ?‹¤.
+	// PostCondition	: onì˜ ê°’ì— ë”°ë¼ ì°¨ëŸ‰ì˜ ì „ë°© ë¼ì´íŠ¸ê°€ On ë˜ëŠ” Offê°€ ëœë‹¤.
 	// Return 		: none
 	void frontLightOnOff(unsigned short lightFlag, int on);
 
 	// rearLightOnOff
-	// ì°¨ëŸ‰?˜ ?›„ë°? ?¼?´?Š¸ë¥? On, Off ?•´ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
+	// ì°¨ëŸ‰ì˜ í›„ë°© ë¼ì´íŠ¸ë¥¼ On, Off í•´ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: on?˜ ê°’ì— ?”°?¼ ì°¨ëŸ‰?˜ ?›„ë°? ?¼?´?Š¸ê°? On ?˜?Š” Offê°? ?œ?‹¤.
+	// PostCondition	: onì˜ ê°’ì— ë”°ë¼ ì°¨ëŸ‰ì˜ í›„ë°© ë¼ì´íŠ¸ê°€ On ë˜ëŠ” Offê°€ ëœë‹¤.
 	// Return 		: none
 	void rearLightOnOff(unsigned short lightFlag, int on);
 
 	// auto_speedMapping
-	// ì°¨ëŸ‰?˜ ?• ë°”í?´ì˜ ê°ë„?— ?”°?¼ ?†?„ë¥? ì¡°ì ˆ?•´ì£¼ëŠ” ?•¨?ˆ˜?´?‹¤.
+	// ì°¨ëŸ‰ì˜ ì• ë°”í€´ì˜ ê°ë„ì— ë”°ë¼ ì†ë„ë¥¼ ì¡°ì ˆí•´ì£¼ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: ì§ì§„ ì£¼í–‰?´?¼ê³? ?Œ?‹¨?•˜ë©? basicSpeedë¥? ?œ ì§??•˜ë©´ì„œ ì£¼í–‰?•œ?‹¤.
-	//                    ê³¡ì„  ì£¼í–‰?´?¼ê³? ?Œ?‹¨?•˜ë©? steerVal?— ?”°?¼ basicSpeed?— ê°?ì¤‘ì¹˜ê°? ë¶??—¬?œ?‹¤.
-	// Return 		: steerVal?— ?”°?¼ ì£¼í–‰?•´?•¼ ?•  ì°¨ëŸ‰?˜ ?†?„ë¥? ë°˜í™˜?•œ?‹¤.
+	// PostCondition	: ì§ì§„ ì£¼í–‰ì´ë¼ê³  íŒë‹¨í•˜ë©´ basicSpeedë¥¼ ìœ ì§€í•˜ë©´ì„œ ì£¼í–‰í•œë‹¤.
+	//                    ê³¡ì„  ì£¼í–‰ì´ë¼ê³  íŒë‹¨í•˜ë©´ steerValì— ë”°ë¼ basicSpeedì— ê°€ì¤‘ì¹˜ê°€ ë¶€ì—¬ëœë‹¤.
+	// Return 		: steerValì— ë”°ë¼ ì£¼í–‰í•´ì•¼ í•  ì°¨ëŸ‰ì˜ ì†ë„ë¥¼ ë°˜í™˜í•œë‹¤.
 	int auto_speedMapping(int steerVal, const int basicSpeed);
 
 	// buzzer
-	// ì°¨ëŸ‰?˜ horn?„ ?š¸ë¦¬ê²Œ ?•˜?Š” ?•¨?ˆ˜?´?‹¤.
+	// ì°¨ëŸ‰ì˜ hornì„ ìš¸ë¦¬ê²Œ í•˜ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: ?›?•˜?Š” ?‹œê°„ë§Œ?¼ buzzerê°? ?š¸ë¦¬ê³ , numOfTimesë§Œí¼ ë°˜ë³µ?•œ?‹¤.
+	// PostCondition	: ì›í•˜ëŠ” ì‹œê°„ë§Œí¼ buzzerê°€ ìš¸ë¦¬ê³ , numOfTimesë§Œí¼ ë°˜ë³µí•œë‹¤.
 	// Return 		: none
 	void buzzer(int numOfTimes, int interval_us, int pulseWidth_us);
 
 	// manualControl
-	// keyë¥? ?…? ¥ë°›ì•„?„œ ?›?•˜?Š” ?™?‘?„ ?•˜ê²? ?•˜?Š” ?•¨?ˆ˜?´?‹¤.
+	// keyë¥¼ ì…ë ¥ë°›ì•„ì„œ ì›í•˜ëŠ” ë™ì‘ì„ í•˜ê²Œ í•˜ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
-	// PostCondition	: ?…? ¥ key?— ?”°?¼ ?•´?‹¹ ?™?‘?´ ?‹¤?–‰?œ?‹¤.
+	// PostCondition	: ì…ë ¥ keyì— ë”°ë¼ í•´ë‹¹ ë™ì‘ì´ ì‹¤í–‰ëœë‹¤.
 	// Return 		: none
 	void manualControl(struct ControlData *cdata, char key);
 
 	// timeCheck
-	// ?›?•˜?Š” êµ¬ê°„ ?‚¬?´?— ê±¸ë¦¬?Š” ?‹œê°„ì„ êµ¬í•˜?Š” ?•¨?ˆ˜?´?‹¤.
+	// ì›í•˜ëŠ” êµ¬ê°„ ì‚¬ì´ì— ê±¸ë¦¬ëŠ” ì‹œê°„ì„ êµ¬í•˜ëŠ” í•¨ìˆ˜ì´ë‹¤.
 	// PreCondition 	: none
 	// PostCondition	: none
-	// Return 		: ?˜„?¬ê¹Œì?? ì¸¡ì •?•œ ?‹œê°„ì„ ë°˜í™˜?•œ?‹¤.
+	// Return 		: í˜„ì¬ê¹Œì§€ ì¸¡ì •í•œ ì‹œê°„ì„ ë°˜í™˜í•œë‹¤.
 	uint32_t timeCheck(struct timeval *tempTime);
 
 	// laneChange
 	// PreCondition		: none
-	// PostCondition	: 0?´ë©? ì¢ŒíšŒ? „, 1?´ë©? ?š°?šŒ? „ ?™?‘?„ ?•˜?Š” ?•¨?ˆ˜.
+	// PostCondition	: 0ì´ë©´ ì¢ŒíšŒì „, 1ì´ë©´ ìš°íšŒì „ ë™ì‘ì„ í•˜ëŠ” í•¨ìˆ˜.
+	//
 	// Return 			: none
 	void laneChange(int direction, int speed, int length);
 
